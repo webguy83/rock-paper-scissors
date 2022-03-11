f3e358cfa434c8238421935bdd081f912fd3f17a
Curtis <curtis_102@hotmail.com>
2022-03-11 00:51:31 -0800

    added pieces

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,5 +1,10 @@
 import Box from '@mui/material/Box';
 import pentagon from '../images/bg-pentagon.svg';
+import lizardIcon from '../images/icon-lizard.svg';
+import paperIcon from '../images/icon-paper.svg';
+import rockIcon from '../images/icon-rock.svg';
+import scissorsIcon from '../images/icon-scissors.svg';
+import spockIcon from '../images/icon-spock.svg';
 import Piece from './Piece';
 
 export default function Gameboard() {
@@ -20,61 +25,32 @@ export default function Gameboard() {
         }}
         position='relative'
       >
-        <Box
-          sx={{
-            width: 130,
-            height: 130,
-            borderRadius: '50%',
-            backgroundColor: 'yellow',
-            position: 'absolute',
-            top: 60,
-            left: -50,
-          }}
-        ></Box>
-        <Box
-          sx={{
-            width: 130,
-            height: 130,
-            borderRadius: '50%',
-            backgroundColor: 'green',
-            position: 'absolute',
-            top: -40,
-            left: 100,
-          }}
-        ></Box>
-        <Box
-          sx={{
-            width: 130,
-            height: 130,
-            borderRadius: '50%',
-            backgroundColor: 'red',
-            position: 'absolute',
-            top: 60,
-            right: -50,
-          }}
-        ></Box>
-        <Box
-          sx={{
-            width: 130,
-            height: 130,
-            borderRadius: '50%',
-            backgroundColor: 'orange',
-            position: 'absolute',
-            top: 240,
-            left: 0,
-          }}
-        ></Box>
-        <Box
-          sx={{
-            width: 130,
-            height: 130,
-            borderRadius: '50%',
-            backgroundColor: 'pink',
-            position: 'absolute',
-            top: 240,
-            right: 0,
-          }}
-        ></Box>
+        <Piece
+          coords={{ top: -40, left: 100 }}
+          colours={{ darker: 'hsl(39, 89%, 49%)', lighter: 'hsl(40, 84%, 53%)' }}
+          icon={scissorsIcon}
+        />
+        <Piece
+          coords={{ top: 60, right: -50 }}
+          colours={{ darker: 'hsl(230, 89%, 62%)', lighter: 'hsl(230, 89%, 65%)' }}
+          icon={paperIcon}
+        />
+        <Piece
+          coords={{ top: 240, right: 0 }}
+          colours={{ darker: 'hsl(349, 71%, 52%)', lighter: 'hsl(349, 70%, 56%)' }}
+          icon={rockIcon}
+        />
+        <Piece
+          coords={{ top: 240, left: 0 }}
+          colours={{ darker: 'hsl(261, 73%, 60%)', lighter: 'hsl(261, 72%, 63%)' }}
+          icon={lizardIcon}
+        />
+        <Piece
+          coords={{ top: 60, left: -50 }}
+          colours={{ darker: 'hsl(189, 59%, 53%)', lighter: 'hsl(189, 58%, 57%)' }}
+          icon={spockIcon}
+        />
+
         <img src={pentagon} alt='pentagon background' />
       </Box>
     </Box>
--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,5 +1,65 @@
 import Box from '@mui/material/Box';
 
-export default function Piece() {
-  return <Box></Box>;
+const mainStyle = {
+  width: 130,
+  height: 130,
+  borderRadius: '50%',
+};
+
+export default function Piece({ coords, colours, icon }) {
+  const HSLvals = colours.darker.replace(/(hsl\(|\s|%|\))/g, '').split(',');
+  const darkL = parseInt(HSLvals[2]) - 10;
+  const darkestColour = `hsl(${HSLvals[0]}, ${HSLvals[1]}%, ${darkL}%)`;
+  return (
+    <Box sx={{ ...coords, position: 'absolute' }}>
+      <Box position='relative'>
+        <Box
+          sx={{
+            position: 'absolute',
+            zIndex: 2,
+            backgroundColor: '#d3d3d3',
+            width: 90,
+            height: 90,
+            borderRadius: '50%',
+            top: '15%',
+            left: '15%',
+          }}
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            zIndex: 3,
+            backgroundColor: 'white',
+            width: 90,
+            height: 90,
+            borderRadius: '50%',
+            top: '19%',
+            left: '15%',
+          }}
+        >
+          <img style={{ width: '50%' }} src={icon} alt='icon' />
+        </Box>
+        <Box
+          sx={{
+            ...mainStyle,
+            position: 'relative',
+            zIndex: 1,
+            background: `linear-gradient(${colours.lighter}, ${colours.darker});`,
+          }}
+        />
+        <Box
+          sx={{
+            ...mainStyle,
+            backgroundColor: darkestColour,
+            position: 'absolute',
+            top: 7,
+            zIndex: 0,
+          }}
+        />
+      </Box>
+    </Box>
+  );
 }
